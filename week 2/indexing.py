a3bd989bb3e2ac49f6c1076a95cef91b8b4aa723
SOURADEEP-DONNY <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-06-23 17:24:07 +0530

    Add files via upload

---
+++ week 2/indexing.py
@@ -0,0 +1,9 @@
+#STRING AND LIST INDEXING
+st="SMARTPHONE"
+list1=['moto','mi','nokia','samsung']
+
+#PRINTING THE LIST IN REVERSE ORDER
+print(list1[-1::-1])
+
+#PRINTING THE STRING IN REVERSE ORDER
+print(st[-1::-1])
