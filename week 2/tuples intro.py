15cd67a42191161234f9fa89e9a471f331e1cd6e
SOURADEEP-DONNY <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-06-23 17:09:54 +0530

    Add files via upload

---
+++ week 2/tuples intro.py
@@ -0,0 +1,14 @@
+#TUPLES ARE JUST LIKE LISTS.
+#DIFFERENCE BETWEEN TUPLES AND LISTS IS TUPLES ARE IMMUTABLE & LISTS ARE MUTABLE.
+# i.e. ONCE DECLARED TUPLES CANNOT BE CHANGED.
+
+mylist=["one",2,"three"]
+mytuple=("one",2,"three")
+
+#----PRINTING THE TYPES-----------
+print(type(mylist))
+print(type(mytuple))
+
+#-----PRINTING THE VALUES----------
+print(mylist)
+print(mytuple)
