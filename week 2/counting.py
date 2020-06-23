30d5a7c2e781a02f48e0834469f0394c09fa4149
SOURADEEP-DONNY <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-06-23 19:45:38 +0530

    Add files via upload

---
+++ week 2/counting.py
@@ -0,0 +1,9 @@
+s="Steganography is basically hiding the pixels inside other pixels and the hidden pixels can be retrieved by reversing the algorithm which was designed to make the encryption algorithm possible."
+
+
+#LETTER COUNTING
+print(s.count('s'))
+
+
+#WORD COUNTING
+print(s.count('the'))
