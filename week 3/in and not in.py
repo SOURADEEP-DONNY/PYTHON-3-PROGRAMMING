561bc73ede0d1a87aa8867cc40dba78794da8709
SOURADEEP-DONNY <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-06-26 11:54:49 +0530

    Add files via upload

---
+++ week 3/in and not in.py
@@ -0,0 +1,29 @@
+# 'in' & 'not in' FUNTIONS
+
+print('a' in 'pineapple')
+print('z' in 'pineapple')
+print('' in 'pineapple')
+
+#THOUGH 'a' IS PRESENT BUT STILL AN ITEM IN THE LIST NEEDS TO BE PRESENT FOR THE EXPRESSION TO BE TRUE
+
+print('a' in ['apple', 'altro', 'pineapple', 'BMW'])
+print('a' in ['apple', 'altro', 'pineapple'])
+
+#CLEARLY AN ITEM NEEDS TO MATCH IN THE LIST TO BE TRUE
+
+print('apple' in ['apple', 'altro', 'pineapple'])
+print('apple' in ['apple', 'altro', 'pineapple', 'BMW'])
+
+
+print('a' not in 'pineapple')
+print('z' not in 'pineapple')
+print('' not in 'pineapple')
+
+
+
+print('a' not in ['apple', 'altro', 'pineapple', 'BMW'])
+print('a' not in ['apple', 'altro', 'pineapple'])
+
+
+print('apple' not in ['apple', 'altro', 'pineapple'])
+print('apple' not in ['apple', 'altro', 'pineapple', 'BMW'])
