e6cfd994a482d4631a3722cc05c1e2c8e399bce2
SOURADEEP-DONNY <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-06-27 20:10:31 +0530

    Add files via upload

---
+++ week 4/CODES.py
@@ -0,0 +1,12 @@
+ael = "python!"
+app=[]
+for _ in ael:
+    app.append(_)
+    
+print(app)
+wrds = ["end", 'work', "play", "start", "walk", "look", "open", "rain", "learn", "clean"]
+past_wrds=[]
+for q in wrds:
+    past_wrds.append(q+'ed')
+    
+print(past_wrds)
