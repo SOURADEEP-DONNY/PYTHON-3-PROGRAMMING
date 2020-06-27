2a87acb92b66087a15ec59cdba3c087b7a620d31
SOURADEEP-DONNY <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-06-27 13:28:33 +0530

    Add files via upload

---
+++ week 4/CODES1.py
@@ -0,0 +1,17 @@
+sports = ['cricket', 'football', 'volleyball', 'baseball', 'softball', 'track and field', 'curling', 'ping pong', 'hockey']
+sports.insert(2,'horseback riding')
+print(sports)
+trav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathmandu', 'Osaka', 'London', 'Melbourne']
+del trav_dest[-2]
+print(trav_dest)
+trav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathmandu', 'Osaka', 'Melbourne']
+trav_dest.append('Guadalajara')
+print(trav_dest)
+winners = ['Kazuo Ishiguro', 'Rainer Weiss', 'Youyou Tu', 'Malala Yousafzai', 'Alice Munro', 'Alvin E. Roth']
+winners.sort()
+print(winners)
+winners = ['Alice Munro', 'Alvin E. Roth', 'Kazuo Ishiguro', 'Malala Yousafzai', 'Rainer Weiss', 'Youyou Tu']
+z_winners=winners
+z_winners.sort()
+z_winners.reverse()
+print(z_winners)
