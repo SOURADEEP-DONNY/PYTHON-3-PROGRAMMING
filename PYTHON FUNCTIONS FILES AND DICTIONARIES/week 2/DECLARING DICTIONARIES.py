107beb57111797c5fb3ca81d643b48c006783146
SOURADEEP-DONNY <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-06-29 22:27:38 +0530

    Add files via upload

---
+++ PYTHON FUNCTIONS FILES AND DICTIONARIES/week 2/DECLARING DICTIONARIES.py
@@ -0,0 +1,22 @@
+#DECLARING DICTIONARIES
+#TYPE 1
+
+dict1={}
+dict1['NAME']='SOURADEEP'
+dict1['ROLL NUMBER']='ECE_17'
+dict1['STREAM']='ELECTRONICS & COMMUNICATION ENGINEERING'
+print(dict1)
+
+
+print('\n\n')
+
+
+#TYPE 2
+DICT={'ONE':1,'TWO':2,'THREE':3}
+print(DICT)
+
+print('\n\n')
+
+# IN ORDER TO GET A VALUE, WE NEED TO CALL THE KEY.
+value=dict1['NAME']
+print(value)
