a669579fca3833bd8e6fd8c7499e958f6fddb251
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-03 12:19:50 +0530

    Add files via upload

---
+++ PYTHON FUNCTIONS FILES AND DICTIONARIES/week 4/BREAK & CONTINUE STATEMENTS.py
@@ -0,0 +1,20 @@
+#BREAK STATEMENT
+i=0
+while i<=10:
+    if i==5:
+        break
+    print(i)
+    i=i+1
+
+#WHEN THE CONDITION MATCHES THE BREAK STATEMENT THE PROGRAM COMES OUT FROM THE LOOP CONDITION.
+print('\n\n')
+
+#CONTINUE STATEMENT
+i=0
+while i<=10:
+    if i==5:
+        continue
+    print(i)        
+    i=i+1
+    
+
