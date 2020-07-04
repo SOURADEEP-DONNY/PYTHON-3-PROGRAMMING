fb7c7629f02da2929421a514ebd7b4dfad46eccc
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-04 22:56:12 +0530

    Add files via upload

---
+++ PYTHON FUNCTIONS FILES AND DICTIONARIES/week 5/REVERSE SORTING.py
@@ -0,0 +1,19 @@
+#REVERSE SORTING
+
+l1=[2,1,999,45,5,67,88,4,987]
+l4=sorted(l1,reverse=True)
+print(l4)
+
+l2=['SOURADEEP','DONNY','TABLE TENNIS','ENGINEERING']
+l3=sorted(l2, reverse=True)
+print(l3)
+
+
+#AGAIN NOT REVERSED
+l1=[2,1,999,45,5,67,88,4,987]
+l4=sorted(l1,reverse=False)
+print(l4)
+
+l2=['SOURADEEP','DONNY','TABLE TENNIS','ENGINEERING']
+l3=sorted(l2, reverse=False)
+print(l3)
