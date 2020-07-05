202d1092a16fd35e0a10f7a4380451e06540b78a
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-05 11:04:43 +0530

    Add files via upload

---
+++ PYTHON FUNCTIONS FILES AND DICTIONARIES/week 5/TUPLE SORTING.py
@@ -0,0 +1,17 @@
+#SORTING TUPLES
+
+t=[('C',6,8),
+   ('Y',2,4),
+   ('C',7,10),
+   ('B',9,0),
+   ('Z',1,2),
+   ('G',4,5),
+   ('B',1,2),
+   ('C',7,8),
+   ('A',8,9)]
+
+for t in sorted(t):
+    print(t)
+
+
+#CLEARLY IN TUPLE SORTING THE SORTING IS DONE ACCORDING TO THE FIRST ELEMENT.
