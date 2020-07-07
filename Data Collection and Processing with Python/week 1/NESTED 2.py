0cb27f3886ba08c713f5eac66ac1c1dcb735b746
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-07 19:48:58 +0530

    Add files via upload

---
+++ Data Collection and Processing with Python/week 1/NESTED 2.py
@@ -0,0 +1,10 @@
+#WAP TO STORE THE LAST NAME IN A LIST last_name
+
+
+
+info=[['SOURADEEP','DAS',1997,'KOLKATA'],['SUKLA','DAS',1965,'KOLKATA'],['SATTYAK','SARMA',1997,'DURGAPUR']]
+lst=[]
+for i in info:
+    lst.append(i[1])
+
+print(lst)
