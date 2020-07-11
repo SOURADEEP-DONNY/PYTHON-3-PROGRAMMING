6555e8cf37dacd451e017b8e73007eb31ab57113
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-12 00:34:10 +0530

    Add files via upload

---
+++ Python Classes and Inheritance/week 1/DEFINING A CLASS.py
@@ -0,0 +1,13 @@
+#=================DEFINNING A CLASS==============================================
+
+class point:   #DECLARED A CLASS
+    pass
+
+point1=point()
+point2=point()
+
+point1.x=10
+point2.x=5
+
+print(point1.x)
+print(point2.x)
