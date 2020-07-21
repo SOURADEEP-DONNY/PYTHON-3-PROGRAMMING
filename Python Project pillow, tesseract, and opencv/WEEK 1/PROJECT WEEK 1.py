9f52c58947eb5c5515d357a9b025906768ca479c
SOURADEEP DAS <66508708+SOURADEEP-DONNY@users.noreply.github.com>
2020-07-21 16:26:36 +0530

    Add files via upload

---
+++ Python Project pillow, tesseract, and opencv/WEEK 1/PROJECT WEEK 1.py
@@ -0,0 +1,51 @@
+VOWEL_COST = 250
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+VOWELS = 'AEIOU'
+class WOFPlayer:
+    prizeMoney = 0
+    prizes = []
+
+    def __init__(self, name):
+        self.name = name
+
+    def addMoney(self, amt):
+        self.prizeMoney += amt
+
+    def goBankrupt(self):
+        self.prizeMoney = 0
+
+    def addPrize(prizes,prize):
+        prizes.append(prize)
+
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.prize)
+class WOFHumanPlayer(WOFPlayer):
+    def getMove(self, category, obscuredPhrase, guesse):
+        input(
+            "{%s} has ${%s}\n"
+            "Category: {%s}\n"
+            "Phrase:  {%s}\n"
+            "Guessed: {%s}\n"
+            "Guess a letter, phrase, or type 'exit' or 'pass':\n") % (
+            self.name, self.prizeMoney, category, obscuredPhrase, guesse)
+        return ("%s") % (guesse)
+class WOFComputerPlayer(WOFPlayer):
+    SORTED_FREQUENCIES = "ZQXJKVBPYGFWMUCLDRHSNIOATE"
+    VOWEL_COST = 250
+    VOWELS = "AEIOU"
+
+    def __init__(self, difficulty):
+        self.difficulty = difficulty
+
+    def smartCoinFlip(self):
+        random_num = random.randint(1, 10)
+        if random_num > self.difficulty:
+            return True
+        else:
+            return False
+
+    def getPossibleLetters(self, guessed):
+        return guessed.upper()
+
+    def getMove(category, obscuredPhrase, guessed):
+        return getPossibleLetters(guessed)
